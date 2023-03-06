fa624431a5d64402a4ab8c6371be05b468d38405
Juan Diaz <paradyse@MacBook-Pro-de-Juan.local>
2023-03-06 13:31:50 -0800

    agregue practica 8, y elimine las carpetas restantes

--- practice08/chronometer/android/app/src/main/kotlin/com/example/chronometer/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.chronometer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- practice08/cronometer/cronometer/android/app/src/main/kotlin/com/example/cronometer/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.cronometer
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
